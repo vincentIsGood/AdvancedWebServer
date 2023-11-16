14c65363e644fb69096002a3632ad17a844b46ee
vincentIsGood <vincentdev324@gmail.com>
2023-11-16 16:30:22 +0800

    tunneling gets more options for customization

--- src/com/vincentcodes/webserver/util/TunnelUtils.java
+++ src/com/vincentcodes/webserver/util/TunnelUtils.java
@@ -5,10 +5,20 @@ import com.vincentcodes.webserver.component.response.ResponseBuilder;
 public class TunnelUtils {
     /**
      * @param remoteDest remote server (eg. api.com, api.com:1234, 127.0.0.1:1234)
-     * @param exchangeRequest whether the request will be sent to remote server
+     * @param requestPolicy either "exchange" or "one-way"
      */
-    public static void setupTunnel(ResponseBuilder res, String remoteDest, boolean exchangeRequest){
+    public static void setupTunnel(ResponseBuilder res, String remoteDest, String requestPolicy){
         res.getHeaders().add("X-Vws-Raw-Tunnel", remoteDest);
-        res.getHeaders().add("X-Vws-Exchange-Once", Boolean.toString(exchangeRequest));
+        res.getHeaders().add("X-Vws-Req-Policy", requestPolicy);
+    }
+    /**
+     * @param dangerous whether ssl checks cert or not
+     * @see #setupTunnel(ResponseBuilder, String, String)
+     */
+    public static void setupTunnel(ResponseBuilder res, String remoteDest, String requestPolicy, boolean ssl, boolean dangerous){
+        res.getHeaders().add("X-Vws-Raw-Tunnel", remoteDest);
+        res.getHeaders().add("X-Vws-Req-Policy", requestPolicy);
+        res.getHeaders().add("X-Vws-Ssl-Tunnel", Boolean.toString(ssl));
+        res.getHeaders().add("X-Vws-Ssl-Tunnel-Danger", Boolean.toString(dangerous));
     }
 }
